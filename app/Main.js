1bf864bdd0e741fc042f554317db5f4be0ececcd
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2022-07-04 18:31:20 +0800

    react router
    added router in file

--- app/Main.js
+++ app/Main.js
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Components
 import Header from "./components/Header";
 import Homeguest from "./components/Homeguest";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
 function Main() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Homeguest />
+      <Routes>
+        <Route path="/" element={<Homeguest />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
