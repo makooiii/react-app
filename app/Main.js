3df6ae92f61d9f1fd093d36066e4283db10be4b8
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2022-07-05 21:34:19 +0800

    added home
    lift the loggedIn and setLoggin to the Main.js added the jsx file Home to display home screen after login.

--- app/Main.js
+++ app/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -8,13 +8,16 @@ import Homeguest from "./components/Homeguest";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Terms from "./components/Terms";
+import Home from "./components/Home";
 
 function Main() {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("reactAppToken")));
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Routes>
-        <Route path="/" element={<Homeguest />} />
+        <Route path="/" element={loggedIn ? <Home /> : <Homeguest />} />
         <Route path="/about-us" element={<About />} />
         <Route path="/terms" element={<Terms />} />
       </Routes>
