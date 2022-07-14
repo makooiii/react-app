37f99e6ce873984103d6801211b402057abc6ef4
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2022-07-15 01:50:12 +0800

    viewing specific post

--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -8,7 +8,7 @@ function ProfilePosts() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    async function fetchPost() {
+    async function fetchPosts() {
       try {
         const response = await Axios.get(`/profile/${username}/posts`);
         setPosts(response.data);
@@ -17,7 +17,7 @@ function ProfilePosts() {
         console.log("There Was a problem");
       }
     }
-    fetchPost();
+    fetchPosts();
   }, []);
 
   if (isLoading) return <div>Loading....</div>;
@@ -26,10 +26,10 @@ function ProfilePosts() {
     <div className="list-group">
       {posts.map(post => {
         const date = new Date(post.createdDate);
-        const dataFormatted = `${date.getMonth() + 1} / ${date.getDate()}/${date.getFullYear()}`;
+        const dateFormatted = `${date.getMonth() + 1} / ${date.getDate()}/${date.getFullYear()}`;
         return (
           <Link key={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
-            <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> <span className="text-muted small"> {dataFormatted} </span>
+            <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> <span className="text-muted small"> {dateFormatted} </span>
           </Link>
         );
       })}
