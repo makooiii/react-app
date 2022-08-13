9bcd652e772f021fd438d55b63689439e9ae63b8
Mark Sapiandante <msapiandante.ccheetah@gmail.com>
2022-08-13 17:45:54 +0800

    create edit jsx and its function to display data

--- app/Main.js
+++ app/Main.js
@@ -19,6 +19,7 @@ import CreatePost from "./components/CreatePost";
 import ViewSinglePost from "./components/ViewSinglePost";
 import FlashMessages from "./components/FlashMessages";
 import Profile from "./components/Profile";
+import EditPost from "./components/EditPost";
 
 function Main() {
   const inittialState = {
@@ -70,6 +71,7 @@ function Main() {
             <Route path="/terms" element={<Terms />} />
             <Route path="/create-post" element={<CreatePost />} />
             <Route path="/post/:id" element={<ViewSinglePost />} />
+            <Route path="/post/:id/edit" element={<EditPost />} />
             <Route path="/profile/:username/*" element={<Profile />} />
           </Routes>
           <Footer />
